ff18793b9d1a8e5dae98fee43cc55ea31d89abac
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-04-15 02:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 //your JS code here. If required.
+const body = document.querySelector(body);
 const domLoadSuccess = document.createElement("div");
 domLoadSuccess.innerText="DOM load success";
+body.appendChild(domLoadSuccess);
